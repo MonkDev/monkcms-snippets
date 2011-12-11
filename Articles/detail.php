1d263080d73f6c3250dc75a8f8d916036190b51b
Etienne de Bruin <et@d.ebru.in>
2011-12-10 20:22:03 -0800

    Added Article Detail

---
+++ Articles/detail.php
@@ -0,0 +1,19 @@
+<?php
+
+  /*
+   * Display Article Detail 
+   */
+	  
+  getContent(
+  "article",
+  "display:detail",
+  "find:".$_GET['sermonslug'],
+  "show:<h3>__title__</h3>",
+  "show:<ul id="byline">",
+  "show:<li id="bl_preacher">__author__</li>",
+  "show:<li id="bl_date">__date format='M j, Y'__</li>",
+  "show:<li id="bl_series">Series: __series__</li>",
+  "show:</ul>",
+  "show:<div id="text">__text__</div>"
+  );
+?>

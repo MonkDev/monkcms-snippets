b2792d488d0ef255f6747da337d57a575e795e6e
Etienne de Bruin <et@d.ebru.in>
2011-12-11 20:34:22 -0800

    Fixed the quotation of the classes within the show's.

--- Articles/list.php
+++ Articles/list.php
@@ -1,19 +1,12 @@
 <? 
   /*
    * Article List
+   * Note the various CSS classes are included only for the sake of example.
    */
-  getContent(
-  "page",
-  "find:".$_GET['nav'],
-  "show:<h3>__title__</h3>",
-  "show:<div id="text">__text__</div>"
-  );
-  ?>
-
   <? getContent(
   "article",
   "display:list",
-  "show:<div class="articlebox"><h4 class="title">__titlelink__</h4>",
+  "show:<div class='articlebox'><h4 class='title'>__titlelink__</h4>",
   "show:<p>__date format='m/d/y'__</p>",
   "show:<p>__author__</p>",
   "show:<p>__summary__</p>",

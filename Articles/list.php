f1fda34fb06df45fb679e9c324fb9a1336421aa2
Etienne de Bruin <et@d.ebru.in>
2011-12-11 20:35:55 -0800

    Fixed illegal <?

--- Articles/list.php
+++ Articles/list.php
@@ -3,7 +3,7 @@
    * Article List
    * Note the various CSS classes are included only for the sake of example.
    */
-  <? getContent(
+  getContent(
   "article",
   "display:list",
   "show:<div class='articlebox'><h4 class='title'>__titlelink__</h4>",

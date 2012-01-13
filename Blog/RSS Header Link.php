6bd3384e32f1964b4ea371d54ec6d3ea22743b3a
Benson Lee <benson@monkdevelopment.com>
2012-01-13 15:51:10 -0800

    Some housecleaning - comments & file renaming.

--- Blog/RSS Header Link.php
+++
@@ -1,9 +0,0 @@
-      <? getContent (
-            "blog",
-            "display:auto",
-            "before_show_postlist:<link rel=\"alternate\" type=\"application/rss+xml\"",
-            "before_show_postlist: title=\"RSS Feed\"",
-            "before_show_postlist: href=\"__blogrss__\"",
-            "before_show_postlist: />"
-        );
-        ?>

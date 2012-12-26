a36d4d8c3166dd52533bc83ea24691eed1297392
Chris Ullyott <contact@chrisullyott.com>
2012-12-26 11:05:15 -0800

    Cleaned up hide cached events JS.

--- Events/hide-expired-events.js
+++ Events/hide-expired-events.js
@@ -1,16 +1,16 @@
-// Expressly hide old events from page if stuck in cache
-// In the output (PHP/Monklet) of the event item, add __eventend format='Y-m-d'__ in a data attribute for comparison.
+// Expressly hide old events from page if cached
+// In the output (PHP/Monklet) of the event item, add __eventend format='U'__ in a data attribute for comparison.
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm}
-var today = yyyy + '-' + mm + '-' + dd ;
-$('#eventList .event').each(function(){
-	var this_event = $(this);
-	var event_end = $(this).attr('data-eventend');
-	if (new Date(event_end) < new Date(today)){
-		this_event.hide();
-	}
+$(document).ready(function(){
+
+	var now = new Date().getTime()/1000;
+
+	$('#eventList .event').each(function(){
+		var this_event = $(this);
+		var event_end = this_event.data('eventend');
+		if (parseInt(event_end) < parseInt(now)){
+			this_event.hide();
+		}
+	});
+
 });

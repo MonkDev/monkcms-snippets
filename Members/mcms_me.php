acd30f2e0c617dd2e865e1a0c704d38d5badc713
Chris Ullyott <chris@monkdevelopment.com>
2013-02-28 15:10:05 -0800

    Adjusted mepage.

--- Members/mcms_me.php
+++ Members/mcms_me.php
@@ -1,5 +1,4 @@
 <?php
-	// This is basic mcms_me.php (Me Page) code to facilitate the ability to edit a user password & to view ecommerce transaction receipts 
 
 	getContent(
 	  'member',
@@ -10,18 +9,13 @@
 	  'nocache'
 	);
 
-
-	// display a relevant message depending on whether the user is viewing a transaction receipt 
+	// display a relevant message depending on whether the user is viewing a transaction receipt
 	// or if they are viewing the confirmation page after completing an order.
 	if ($_GET['view'] == 'orders') {
-	  $backLink = '<a href="/me/">&#8249; Back to order list</a> | ';
+	  $backLink = '<a href="/me/">&#8249; Back to order history</a> | ';
 	}
 
-	if ($_GET['thanks']) {
-	  $thanksText = '<p><strong>Thank you for your order! Your order details are below.</strong></p>';
-	}
-
-	// displays a links to edit the user's account or logo out 
+	// displays a links to edit the user's account or logo out
 	getContent(
 	  'member',
 	  'display:summary',
@@ -35,6 +29,16 @@
 	  'nocache'
 	);
 
+	if ($_GET['thanks']) {
+	  $thanksText = '<p><strong>Thank you for your order! Your order details are below.</strong></p>';
+	}
+
+?>
+
+<hr />
+
+<?php
+
 	// grabs a list of links to transactions that have been made on the account. If the user arrives
 	// at the page from completing an order, it displays the order confirmation page.
 	$orderHistory = getContent(
@@ -46,20 +50,9 @@
 	);
 
 	if (strpos($orderHistory, 'orderHistoryNone') === false) {
-	  echo "<h2>Order History</h2>";
+	  echo "<h3>Order Details</h3>";
 	  echo $thanksText;
 	  echo $orderHistory;
-	}	
-	
-	
-	// optionally display the user's profile page on the sidebar
-	getContent(
-	  'member',
-	  'display:summary',
-	  'findid:' . $MCMS_MID,
-	  'show:<div id="sidebar_header_image"></div><img src="__pictureurl__" alt="__fullname__" width="210" class="headerImage" />',
-	  'nocache'
-	);
+	}
+
 ?>
-
-

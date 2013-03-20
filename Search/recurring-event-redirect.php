9ea161132c90f68256c516c57c319dad46e38755
Chris Ullyott <chris@monkdevelopment.com>
2013-03-20 10:42:36 -0700

    Minor update to recurring event redirect.

--- Search/recurring-event-redirect.php
+++ Search/recurring-event-redirect.php
@@ -1,7 +1,7 @@
 <?php
 
 	/*
-		REDIRECT EVENT SEARCH RESULTS TO LATEST EVENT OCCURRENCE
+		REDIRECT RECURRING EVENT SEARCH RESULT TO LATEST OCCURRENCE
 
 		Conditions:
 		1. Event detail pages are created like so: /event/date-event-slug/
@@ -20,7 +20,7 @@
 			// original event string
 			$event_slug_old = $event_slug;
 
-			// get old event's title string
+			// get old event's title
 			$event_preg_match = array();
 			preg_match('/[0-9]{4}-[0-9]{2}-[0-9]{2}/', $event_slug, $event_preg_match);
 			$event_slug = str_replace($event_preg_match[0], '', $event_slug);
@@ -29,11 +29,11 @@
 			$event_slug = str_replace($event_preg_match[0], '', $event_slug);
 			$event_slug = ltrim($event_slug,'-');
 
-			// get updated event's URL
+			// get new event by title
 			$event_url_new = getContent('event','display:detail','find:' . $event_slug,'show:__url__','noecho');
 			$event_slug_new = str_replace('event/','',trim($event_url_new,'/'));
 
-			// if new slug is different, redirect
+			// if new event string is different, redirect to new URL
 			if(($event_slug_old != '') && ($event_slug_new != '') && ($event_slug_old != $event_slug_new)){
 				$event_url_new = 'http://' . $_SERVER['HTTP_HOST'] . $event_url_new;
 				header('Location:' . $event_url_new);

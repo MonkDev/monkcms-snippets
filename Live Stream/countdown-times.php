7e6c266c8d8281c6291368df63658dc621972338
Chris Ullyott <chris@monkdevelopment.com>
2013-06-12 11:35:40 -0700

    Added Ricky's live stream rework.

--- Live Stream/countdown-times.php
+++ Live Stream/countdown-times.php
@@ -3,10 +3,6 @@
 require($_SERVER["DOCUMENT_ROOT"]."/monkcms.php");
 header('Content-type: application/json');
 
-$event_array;
-$nodes;
-$json;
-
 // Set PHP to client time zone
 $timezone = getContent('site','display:detail','show:__timezone__','noecho');
 date_default_timezone_set($timezone);
@@ -15,56 +11,60 @@ date_default_timezone_set($timezone);
 $now = strtotime('now');
 $yesterday = date('m/d/Y',strtotime('-1 day',$now));
 
+
 // Get event data
 $event_string =
 getContent(
-"event",
-"display:list",
-"enablepast:yes",
-"recurring:yes",
-"repeatevent:yes",
-"find_category:live-service",
-"startdate:" . $yesterday,
-"howmany:5",
-"show:__title__", //0
-"show:||",
-"show:__eventstart format='F j, Y G:i:s'__", // 1 - start time
-"show:||",
-"show:__eventstartTwo format='F j, Y'__ __eventend format='G:i:s'__", // 2 - end time
-"show:~~~",
-"noecho"
+    "event",
+    "display:list",
+    "enablepast:yes",
+    "recurring:yes",
+    "repeatevent:yes",
+    "find_category:live-event",
+    "startdate:" . $yesterday,
+    "howmany:15",
+    "show:__title__",
+    "show:||",
+    "show:__eventstart format='F j, Y G:i:s'__", // 1 - start time
+    "show:||",
+    "show:__eventstartTwo format='F j, Y'__ __eventend format='G:i:s'__", // 2 - end time
+    "show:~~~",
+    "noecho"
 );
 
 // Process data string for current events
-$event_prearray = explode("~~~",$event_string);
-for ($i=0; $i<count($event_prearray)-1; $i++) {
+$event_prearray = explode("~~~", trim($event_string, "~~~"));
+
+for ($i=0; $i<count($event_prearray); $i++) {
+
 	$item_arr = explode("||",$event_prearray[$i]);
 	$event_end = $item_arr[2];
+
+    // Include only events that have not yet ended
 	if($now<strtotime($event_end)){
-		$event_array[$i] = $item_arr;
+		$event_array[] = $item_arr;
 	}
+
 }
 
-// Build JSON with time zone via DateTime / DateTimeZone classes.
-$i = 0;
-foreach ($event_array as $key => $value) {
-  $title = $value[0];
-  $start_time = $value[1];
-  $end_time = $value[2];
-  $dateTime_start = new DateTime($start_time, new DateTimeZone($timezone));
-  $dateTime_end = new DateTime($end_time, new DateTimeZone($timezone));
-  $nodes[$i] = array(
-		id => "id".strval($i),
-		title => $title,
-		date => array(
-		   start => $dateTime_start->format('F j, Y G:i:s O'),
-		   end => $dateTime_end->format('F j, Y G:i:s O')
-		),
+// Prepare JSON output
+for ($i=0; $i<count($event_array); $i++) {
+
+    $title = $event_array[$i][0];
+
+    $dateTime_start = new DateTime($event_array[$i][1], new DateTimeZone($timezone));
+    $dateTime_end = new DateTime($event_array[$i][2], new DateTimeZone($timezone));
+
+    $nodes[$i] = array(
+    	id => "id".strval($i),
+    	title => $title,
+    	date => array(
+    	    start => $dateTime_start->format('F j, Y G:i:s O'),
+    	    end => $dateTime_end->format('F j, Y G:i:s O')
+    	),
     );
-  $i++;
-}
 
-//print_r($nodes); // testing
+}
 
 $json = json_encode($nodes);
 echo $json;

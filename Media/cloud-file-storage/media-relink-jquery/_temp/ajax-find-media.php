165272377c8207bf52488eeecdd12b79f4527a2f
Chris Ullyott <chris@monkdevelopment.com>
2013-07-02 12:25:02 -0700

    Adjustments to jQuery file replacement script.

--- Media/cloud-file-storage/media-relink-jquery/_temp/ajax-find-media.php
+++ Media/cloud-file-storage/media-relink-jquery/_temp/ajax-find-media.php
@@ -1,18 +1,24 @@
 <?php
 
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/monkcms.php");
+	require($_SERVER["DOCUMENT_ROOT"] . "/monkcms.php");
 
-	$get_file = trim(getContent(
-		"media",
-		"display:detail",
-		"find:" . $_GET['filename'],
-		"show:__url__",
-		"noecho"
-	));
+	$filename = $_GET['filename'];
 
-	if($get_file!=''){
+	if($filename!=''){
 
-		echo $get_file;
+		$get_file = trim(getContent(
+			"media",
+			"display:detail",
+			"find:" . $filename,
+			"show:__url__",
+			"noecho"
+		));
+
+		if($get_file!=''){
+
+			echo $get_file;
+
+		}
 
 	}
 
--- Media/cloud-file-storage/media-relink-jquery/_temp/media-relink.js
+++ Media/cloud-file-storage/media-relink-jquery/_temp/media-relink.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
 
 	// select media dir and monkserve links
-	$('
-		a[href*="mediafiles"],
-		img[src*="mediafiles"],
-
-		a[href*="media.monkserve.com"],
-		img[src*="media.monkserve.com"]
-
-	').each(function() {
+	$('a[href*="mediafiles"],img[src*="mediafiles"],a[href*="media.monkserve.com"],img[src*="media.monkserve.com"]').each(function() {
 
 		var item = $(this);
 		var href = item.attr('href');
 		var src = item.attr('src');
 
 		if(href!==undefined){
+			//console.log('broken anchor HREF found');
 			if ((href.indexOf("/uploaded/") <= 0) && (href.indexOf(".xml") <= 0)) {
 				var filename = href.split('/')[href.split('/').length - 1];
 				filename = filename.split('.')[0];
@@ -31,6 +25,7 @@ $(document).ready(function() {
 		}
 
 		if(src!==undefined){
+			//console.log('broken image SRC found');
 			if (src.indexOf("/uploaded/") <= 0) {
 				var filename = src.split('/')[src.split('/').length - 1];
 				filename = filename.split('.')[0];

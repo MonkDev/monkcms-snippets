28ce58c25b89889062a6915f0202c12d0df2b05d
Chris Ullyott <contact@chrisullyott.com>
2013-10-28 22:48:49 -0700

    Edit to private-content.php

--- Login/private-content.php
+++ Login/private-content.php
@@ -2,7 +2,7 @@
 
 /* ===================================================================== //
 
-	CUSTOM LOGIN SYSTEM FOR PRIVATE PAGES
+	CUSTOM LOGIN FOR PRIVATE PAGES or BLOGS
 
 	author: Chris Ullyott
 
@@ -10,17 +10,17 @@
 	user is a group member. There are three scenarios:
 
 	1.	The user is not logged in
-	2.	The user is logged in, but not a member
+	2.	The user is logged in, but not yet a member
 	3.	The user is logged in and a member
 
 	*Remember that the Site Group cannot be present in a Page's groups
 	for it to be private.
-	
+
 	Here are some instructions for the user on using a template with these additions:
-	
+
 	1. Create a Group in People > Groups, and set the privacy of the Group to Private.
 	2. Add yourself and/or a few Members to this Group.
-	3. Open the Page you want to make private. Change the Template to Subpage Layout (Private). 
+	3. Open the Page you want to make private. Change the Template to Subpage Layout (Private).
 	4. Assign your private Group to the Page on the Publish screen. Be sure to remove the Site Group.
 	5. In Content > Sections > Access Denied Message, set the message you'd like users to see if they log in, but do not yet have access to the private Group (a CMS user will need to add them).
 
@@ -34,19 +34,27 @@
 
 <?php
 
+	/* MEMBERSHIP */
+
 	$group_access = false;
-	
-	$get_groups = getContent("page", "display:detail", "find:" . $_GET['nav'], "show:__groupslugs__", "noecho"); // this API call will be different for sermons, blogs, etc.
+
+	// Get groups list
+	$get_groups = getContent("blog", "display:auto", "howmany:1", "show_postlist:__groupslug__", "noecho"); // blogs
+	$get_groups = getContent("page", "display:detail", "find:" . $_GET['nav'], "show:__groupslugs__", "noecho"); // pages
+
+	// Sanitize groups list
 	$get_groups = trim(preg_replace('/\s+/', '', $get_groups), ',');
-	
+
+	// Get members of all groups
 	$group_members = getContent("member", "display:list", "find_group:" . $get_groups, "howmany:9999", "show:__username__,", "noecho");
 	$group_members = explode(',', trim($group_members, ','));
 	$group_members = array_values(array_unique($group_members));
-		
+
+	// Check if current member is included
 	if (in_array($MCMS_USERNAME, $group_members)) {
 		$group_access = true;
 	}
-	
+
 ?>
 
 
@@ -54,15 +62,31 @@
 
 <?php
 
-	// If not logged in, show the default private page message.
+	/* HELPERS */
+
+	// If title of Blog can't be returned due to privacy, get it via wildcard.
+	if(!$blogslug){ $blogslug_arr = explode('/',trim($_GET['wildcard'],'/')); $blogslug = $blogslug_arr[0]; }
+
+	function slugToTitle($slug){
+		$title = trim($slug,'/');
+		$title = ucwords(str_replace('-',' ',$title));
+		$title = preg_replace('/usa/i','USA',$title); // handle uniquely capitalized words
+		return $title;
+	}
+
+?>
+
+
+
+
+<?php
+
+	/* NOT LOGGED IN */
 
 	if (!$MCMS_LOGGEDIN) {
 
-		getContent(
-	   "page",
-	   "find:" . $_GET['nav'],
-	   "show:__text__"
-	   );
+		$login_link = getContent("login","display:popup","label:login","noecho");
+		echo '<p>Please ' . $login_link . ' to access this blog.</p>';
 
 	}
 
@@ -73,16 +97,24 @@
 
 <?php
 
-	// If logged in and *not* a group member, show the access denied message.
+	/* LOGGED IN, NOT A MEMBER */
 
 	if ( $MCMS_LOGGEDIN && !$group_access ):
 
 ?>
 
+		<h3>Your account does not have access to this page.</h3>
 
-		<p>Your account does not have access to this page. Please contact the site administrator.</p>
-
-		<p><a href="/">&laquo; home</a></p>
+		<?php
+			// Custom "access denied" message.
+			getContent(
+			"section",
+			"display:detail",
+			"find:access-denied-" . $some_slug, // some identifier (group name, etc)
+			"show:__text__",
+			"<p><a href='/'>&laquo; home</a></p>"
+			);
+		?>
 
 
 <?php endif; ?>
@@ -92,7 +124,7 @@
 
 <?php
 
-	// If logged in *and* a group member, show everything.
+	/* LOGGED IN AND A MEMBER */
 
 	if( $MCMS_LOGGEDIN && $group_access ):
 

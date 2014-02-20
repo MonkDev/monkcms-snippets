840f1d44d9ed486f061de10852838cb881f10cfa
Chris Ullyott <chris@monkdevelopment.com>
2014-02-19 17:33:09 -0800

    Improved accordion JS.

--- Javascript/accordion/accordion.js
+++ Javascript/accordion/accordion.js
@@ -1,40 +1,54 @@
+
+// Accordion
+accordion('.accordiontrigger', '.accordiontext', true);
+
+
 /*============================================
  *
  *                 accordion()
  *
  *=============================================
  *
- * This controls user defined accordions
+ * Controls user-defined accordion content.
  *
- * @return - null
+ * To hide all other open content when an 
+ * accordion link is clicked, set the "linked" 
+ * param to true. 
  *
  */
-function accordion() {
-	var allPanels = $('.accordiontext');
-	$('.accordiontrigger').each(function() {
-		$(this).append('<span class="accordionstatus"> [+]</span>');
-	});
-	$('.accordiontrigger').click(function() {
-		$this = $(this);
-		$target = $this.next('.accordiontext');
-		$(this).toggleClass('active');
-		if (!$target.hasClass('active')) {
-			allPanels.slideUp(function() {
-				allPanels.removeClass('active')
-			});
-			$('.accordionstatus').text(' [+]');
-			$this.find('.accordionstatus').text(' [–]');
-			allPanels.prev('.accordiontrigger').find('.plus-icon').removeClass('hidden');
-			$target.slideDown(function() {
-				$target.addClass('active')
-			});
-			$this.find('.plus-icon').addClass('hidden');
-		} else {
-			$target.slideUp(function() {
-				$target.removeClass('active');
-			});
-			$this.find('.accordionstatus').text(' [+]');
-		}
-		return false;
-	});
+function accordion(triggerClass, contentClass, linked) {
+	if (typeof linked === undefined) {
+		var linked = false;
+	}
+	if ($(triggerClass).length > 0) {
+		$(triggerClass).each(function() {
+			$(this).append('<span class="accordionstatus"> [+]</span>');
+			var contentItems = $(this).nextUntil(triggerClass, contentClass).wrapAll('<div class="accordionbellow">');
+		});
+		$(triggerClass).click(function() {
+			var $this = $(this);
+			var $target = $this.next('.accordionbellow');
+			var $all = $('.accordionbellow');
+			$(this).toggleClass('active');
+			if (!$target.hasClass('active')) {
+				if (linked) {
+					$all.slideUp(function() {
+						$all.removeClass('active')
+					}).prev(triggerClass).find('.plus-icon').removeClass('hidden');
+				}
+				$('.accordionstatus').text(' [+]');
+				$this.find('.accordionstatus').text(' [–]');
+				$target.slideDown(function() {
+					$target.addClass('active')
+				});
+				$this.find('.plus-icon').addClass('hidden');
+			} else {
+				$target.slideUp(function() {
+					$target.removeClass('active');
+				});
+				$this.find('.accordionstatus').text(' [+]');
+			}
+			return false;
+		});
+	}
 }

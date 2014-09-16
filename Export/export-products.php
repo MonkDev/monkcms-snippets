77611543f7346c2ad8bd0e6e4b8bdb0f79bd90be
Chris Ullyott <chris@monkdevelopment.com>
2014-09-16 08:48:19 -0700

    Added new array-formatted API call function.

--- Export/export-products.php
+++ Export/export-products.php
@@ -1,71 +1,132 @@
-<!-- A basic script for exporting a product list to an HTML table -->
+<?php
 
-<? require($_SERVER["DOCUMENT_ROOT"]."/monkcms.php"); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<title>MonkCMS Product Export</title>
-<body>
-<table border="1" cellspacing="6" id="product-list">
-<tr>
-<th>Product Code</th>
-<th>Product Name</th>
-<th>Author</th>
-<th>Current Price</th>
-<th>Original Price</th>
-<th>Type</th>
-<th>Subscription</th>
-<th>Publisher</th>
-<th>Reference</th>
-<th>Program Affiliation</th>
-</tr>
-<?
-getContent(
-   "products",
-   "display:list",
-   "order:title",
-   "family:all",
-   "product:all",
-   "howmany_product:100",
-   "howmany_family:1",
-   //"howmany:100",  
-   //"page:".$_GET['page'],
-   "show_productlist:<tr>",
-   "show_productlist:<td>",
-   "show_productlist:__productcode__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__producttitle__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__skuauthor__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__productprice__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__productOriginalprice__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__type__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__subscription__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__publisher__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__reference__",
-   "show_productlist:</td>",
-   "show_productlist:<td>",
-   "show_productlist:__affiliation__",
-   "show_productlist:</td>",
-   "show_productlist:</tr>",
-   "nocache"
-   );
+	/*
+
+	OUTPUT PRODUCTS AS CSV
+
+	How to use:
+	Upload this file to your site root, and use this URL to download a CSV:
+	http://www.site.com/export-products.php
+
+	*/
+
+
+	$filename = 'products' . 'Export' . date('M') . '_' . date('d') . '_' . date('Y');
+	$howmany = 100; // Set to number of products in the module
+
+
+	// Header
+	header("Content-type: text/csv");
+	header("Content-Disposition: attachment; filename=" . $filename . ".csv");
+	header("Pragma: no-cache");
+	header("Expires: 0");
+
+
+	// MonkCMS
+	require($_SERVER['DOCUMENT_ROOT'] . '/monkcms.php');
+
+
+	// Functions
+	function processItem($in){
+		$out = trim($in);
+		$out = str_replace('"','""',$out);
+		$out = str_replace('&amp;','&',$out);
+		$out = '"' . $out . '"';
+		return $out;
+	}
+
+
+	// Headers
+	$headers .= '"Code",'; 															// 0
+	$headers .= '"Product",'; 													// 1
+	$headers .= '"Price",'; 														// 2
+	$headers .= '"Family",'; 														// 3
+	$headers .= '"Image 1",'; 													// 4
+	$headers .= '"Image 2",'; 													// 5
+	$headers .= '"Image 3",'; 													// 6
+	$headers .= "\n";
+
+	// Lines
+	$batch_length = 50;
+	$batch_count = ceil($howmany / $batch_length);
+
+	$get_products = '';
+	for($i=1; $i<=$batch_count; $i++){
+
+		$this_howmany = strval($batch_length);
+		$this_offset = strval(($batch_length * ($i-1)));
+
+		$get_products .=
+		getContent(
+		"product",
+		"display:list",
+		"order:recent",
+		"howmany_product:".$this_howmany,
+		"offset_product:".$this_offset,
+		"show_productlist:__productcode__", 							// 0
+		"show_productlist:~||~",
+		"show_productlist:__producttitle__",							// 1
+		"show_productlist:~||~",
+		"show_productlist:__productprice__",							// 2
+		"show_productlist:~||~",
+		"show_productlist:__familytitle__",								// 3
+		"show_productlist:~||~",
+		"show_productlist:__productimageURL__",						// 4
+		"show_productlist:~||~",
+		"show_productlist:__productimageURL2__",					// 5
+		"show_productlist:~||~",
+		"show_productlist:__productimageURL3__",					// 6
+		"show_productlist:~|~|~",
+		"noecho"
+		);
+
+	}
+
+	$get_products_array = explode("~|~|~", $get_products);
+
+	for($i=0;$i<count($get_products_array)-1;$i++){
+
+		$event_array = explode("~||~",$get_products_array[$i]);
+
+		$line =
+		processItem($event_array[0]) . "," .
+		processItem($event_array[1]) . "," .
+		processItem($event_array[2]) . "," .
+		processItem($event_array[3]) . "," .
+		processItem($event_array[4]) . "," .
+		processItem($event_array[5]) . "," .
+		processItem($event_array[6]) . "," .
+		processItem($event_array[7]) . "," .
+		processItem($event_array[8]) . "," .
+		processItem($event_array[9]) . "," .
+		processItem($event_array[10]) . "," .
+		processItem($event_array[11]) . "," .
+		processItem($event_array[12]) . "," .
+		processItem($event_array[13]) . "," .
+		processItem($event_array[14]) . "," .
+		processItem($event_array[15]) . "," .
+		processItem($event_array[16]) . "," .
+		processItem($event_array[17]) . "," .
+		processItem($event_array[18]) . "," .
+		processItem($event_array[19]) . "," .
+		processItem($event_array[20]) . "," .
+		processItem($event_array[21]) . "," .
+		processItem($event_array[22]) . "," .
+		processItem($event_array[23]) . "," .
+		processItem($event_array[24]) . "," .
+		processItem($event_array[25]) . "," .
+		processItem($event_array[26]) . "," .
+		processItem($event_array[27]) . "," .
+		processItem($event_array[28]) . "," .
+		processItem($event_array[29]) . "\n" ;
+
+		$lines .= $line;
+	}
+
+	$lines = trim($lines,"\n");
+
+
+	// Output
+	echo $headers . $lines;
+
 ?>
-</table>
-</body>
-</html>

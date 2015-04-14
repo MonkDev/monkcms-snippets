4090099be9a98ff0db1d9184dda6438e7f987cd3
Chris Ullyott <chris@monkdevelopment.com>
2015-04-14 08:43:16 -0700

    Added MassDownloader

---
+++ Media/MassDownloader/MassDownloader.py
@@ -0,0 +1,46 @@
+# download2.py - Download many URLs using multiple threads.
+import os
+import re
+import urllib
+import workerpool
+
+class DownloadJob(workerpool.Job):
+	"Job for downloading a given URL."
+	def __init__(self, url):
+		self.path = 'files/'
+		self.url = url # The url we'll need to download when the job runs
+	def run(self):
+		filename = os.path.basename(self.url)
+		filename = re.sub('^(([A-Za-z]|[0-9]|_)+_)', '', filename)
+		filepath = self.path + filename
+		if not os.path.isfile(filepath):
+			save_to = filepath
+			urllib.urlretrieve(self.url, save_to)
+
+# Initialize a pool, 5 threads in this case
+pool = workerpool.WorkerPool(size=5)
+
+# Count the total number of files
+total = 0
+for url in open("urls.txt"):
+	url = url.strip()
+	if(url):
+		total = total + 1
+
+print 'Total files: ' + str(total)
+
+# Loop over urls.txt and create a job to download the URL on each line
+count = 0
+for url in open("urls.txt"):
+	url = url.strip()
+	if url:
+		count = count + 1  
+		job = DownloadJob(url)
+		pool.put(job) 
+		filename = os.path.basename(url).strip()
+		filename = re.sub('^(([A-Za-z]|[0-9]|_)+_)', '', filename)
+		print('Downloading ' + str(count) + ' of ' + str(total) + ' ... ' + filename)
+
+# Send shutdown jobs to all threads, and wait until all the jobs have been completed
+pool.shutdown()
+pool.wait()
---
+++ Media/MassDownloader/README.md
@@ -0,0 +1,25 @@
+# Multi-threaded downloading with Workerpool
+ 
+https://code.google.com/p/workerpool/
+
+chris@monkdevelopment.com
+
+## Instructions for MacOS:
+
+1. Place the folder "MassDownloader" on your Desktop.
+
+2. Open Applications > Utilities > Terminal.
+
+3. In Terminal, type this command (without the dollar sign) and press Enter:
+
+```
+$ cd ~/Desktop/MassDownloader
+```
+
+4. Run this command to begin the download:
+
+```
+$ python MassDownloader.py
+```
+
+5. The files listed in "urls.txt" will be stripped of their unique file ID and start downloading to the "files" folder.

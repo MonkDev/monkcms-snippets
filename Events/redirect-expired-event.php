8e6910c0988ca793445039d1ff18f7e75822e261
Chris Ullyott <chris@monkdevelopment.com>
2015-08-19 14:25:34 -0700

    Corrected PSR-2 formatting on script.

--- Events/redirect-expired-event.php
+++ Events/redirect-expired-event.php
@@ -1,47 +1,45 @@
 <?php
 
-	/*
-		REDIRECT RECURRING EVENT SEARCH RESULT TO LATEST OCCURRENCE
+  /*
 
-		Requirements:
-		1. Event detail pages are created like so: /event/date-event-slug/
-		2. Search results page is: /search-results/?keywords...
+    REDIRECT RECURRING EVENT SEARCH RESULT TO LATEST OCCURRENCE
 
-		To use:
-		Include at the top of the event detail page (most likely ekk_eventpage.php),
-		just after the require of monkcms.php. Verify that headers are not already
-		being sent by an earlier script including blank space, etc.
+    Requirements:
+    1. Event detail pages are created like so: /event/date-event-slug/
+    2. Search results page is: /search-results/?keywords...
 
-		Note:
-		May not return the correct event if the "next" event is an exception to
-		the recurrence rule.
+    To use:
+    Include at the top of the event detail page (most likely ekk_eventpage.php),
+    just after the require of monkcms.php. Verify that headers are not already
+    being sent by an earlier script including blank space, etc.
 
-	*/
+    Note:
+    May not return the correct event if the "next" event is an exception to
+    the recurrence rule.
 
-	function updatedEventRedirect($event_slug){
+  */
 
-		//if (strpos(rtrim($_SERVER['HTTP_REFERER'],'/'),'/search-results') !== false) {
+  function updatedEventRedirect($event_slug)
+  {
 
-			// query for new event
-			$event_slug_new = getContent(
-			  'event',
-			  'display:detail',
-			  'find:' . preg_replace('/(\d{1,10}-)?(\d{4}-\d{2}-\d{2}-)?/', '', $event_slug),
-			  'show:__url__',
-			  'noecho',
-			  'noedit'
-      );
+    // query for new event
+    $event_slug_new = getContent(
+      'event',
+      'display:detail',
+      'find:'.preg_replace('/(\d{1,10}-)?(\d{4}-\d{2}-\d{2}-)?/', '', $event_slug),
+      'show:__url__',
+      'noecho',
+      'noedit'
+    );
+
       $event_slug_new = preg_replace('/^event\//', '', trim($event_slug_new, '/'));
 
-			// if new event is different, redirect to new URL
-			if(($event_slug && $event_slug_new) && ($event_slug != $event_slug_new)){
-				header('Location:' . 'http://' . $_SERVER['HTTP_HOST'] . '/event/' . $event_slug_new . '/');
-			}
+    // if new event is different, redirect to new URL
+    if (($event_slug && $event_slug_new) && ($event_slug != $event_slug_new)) {
+        header('Location:'.'http://'.$_SERVER['HTTP_HOST'].'/event/'.$event_slug_new.'/');
+    }
+  }
 
-		}
-
-	//}
-
-	updatedEventRedirect($_GET['slug']);
+  updatedEventRedirect($_GET['slug']);
 
 ?>

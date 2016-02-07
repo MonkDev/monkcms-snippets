d09f277238f0c76b67a795f82ffa4577f271e855
Chris Ullyott <contact@chrisullyott.com>
2016-02-07 11:02:03 -0800

    new changes to logger

--- Classes/Logger/logger.class.php
+++ Classes/Logger/logger.class.php
@@ -1,57 +1,106 @@
 <?php
 
 /**
- * Logger
- *
- * Methods for keeping logs of site data.
- *
- * @author Chris Ullyott <chris@monkdevelopment.com>
- */
+  * Logger
+  *
+  * Methods for keeping logs of site data.
+  *
+  * @author Chris Ullyott <chris@monkdevelopment.com>
+  */
 class Logger
 {
 
   /**
-   * addArrayToCSV()
-   * Chris Ullyott <chris@monkdevelopment.com>
-   *
-   * Adds an array of data into a hosted CSV file. On first run,
-   * the CSV is created using the array keys as headers.
-   *
-   * If the data stored will be sensitive in nature, be sure to
-   * store this outside of the public web directory so that the
-   * file is not accessible from the site.
-   *
-   * Example:
-   * addArrayToCSV($_GET, 'signups.csv');
-   *
-   * @param array $array An array of data, preferably associative.
-   * @param string $filename The file name, such as "file.csv".
-   * @return null
-   */
-  function addArrayToCSV($array, $file)
+    * addArrayToCSV()
+    * Chris Ullyott <chris@monkdevelopment.com>
+    *
+    * Adds an array of data into a hosted CSV file. On first run,
+    * the CSV is created using the array keys as headers.
+    *
+    * With $dir left empty, the file will be stored in the "logs" path on the
+    * Rackspace Cloud Site. If you will be using a custom path, make sure to
+    * hide the contents from the public web if the data is sensitive.
+    *
+    * Example:
+    * addArrayToCSV($_GET, 'signups.csv');
+    *
+    * @param array $array An array of data, preferably associative.
+    * @param string $file The file name and extension, such as "file.csv".
+    * @param string $dir The full path of a custom directory for the file.
+    * @return null
+    */
+  public static function addArrayToCSV($array, $file, $dir = '')
   {
+      if (!$dir) {
+        $dir = str_replace('/web/content', '/logs', $_SERVER['DOCUMENT_ROOT']);
+      }
+
+      self::createDir($dir);
+      $filePath = self::path($dir, $file);
+
       $time = strtotime('now');
       $array['timestamp'] = date('r', $time);
-      $array['timestamp_u'] = $time;
+      $array['unix_timestamp'] = $time;
 
       $headers = array();
-      if (!file_exists($file)) {
+      if (!file_exists($filePath)) {
           foreach ($array as $k => $val) {
               $headers[] = $k;
           }
       }
 
-      $file = fopen($file, 'a');
-
+      $handle = fopen($filePath, 'a');
       if ($headers) {
-          fputcsv($file, $headers);
+          fputcsv($handle, $headers);
       }
       if ($array) {
-          fputcsv($file, $array);
+          fputcsv($handle, $array);
       }
 
-      fclose($file);
+      fclose($handle);
+
       return;
+  }
+
+  /**
+   * Create a directory (or directory tree), if it doesn't already exist.
+   *
+   * @param string $path The full directory path.
+   * @return null
+   */
+  private static function createDir($path, $perms = 755)
+  {
+      $path = rtrim($path, '/');
+
+      if (!file_exists($path)) {
+          if (mkdir($path, $perms, true)) {
+              chmod($path, $perms);
+          }
+      }
+
+      if (file_exists($path)) {
+          return true;
+      }
+  }
+
+  /**
+   * Build a path from a list of parts
+   *
+   * @return string A string representing the concatenated path.
+   */
+  private static function path()
+  {
+      $path = '';
+
+      foreach (func_get_args() as $key => $p) {
+          if ($key == 0) {
+              $path .= rtrim($p, '/') . '/';
+          } else {
+              $path .= trim($p, '/') . '/';
+          }
+      }
+
+      return rtrim($path, '/');
   }
 
 }

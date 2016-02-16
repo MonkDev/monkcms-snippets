489e8d67c3bb04835c1302075a88957242e18d9c
Chris Ullyott <chris@monkdevelopment.com>
2016-02-16 10:38:02 -0800

    spacing on event redirect script

--- Events/event-website-redirect.php
+++ Events/event-website-redirect.php
@@ -1,33 +1,35 @@
 <?php
 
-	/*
+    /*
 
-	EVENT PAGE REDIRECT
+    EVENT PAGE REDIRECT
 
-	Redirects the event page to a link entered
-	in the "Website" field in the CMS.
+    Redirects the event page to a link entered
+    in the "Website" field in the CMS.
 
-	This script must be run before any content
-	is output on the page.
+    This script must be run before any content
+    is output on the page.
 
-	http://php.net/manual/en/function.header.php
+    http://php.net/manual/en/function.header.php
 
-	*/
+    */
 
-	$website = trim(getContent(
-	    'event',
-	    'display:detail',
-	    'find:' . $_GET['slug'],
-	    'show:__website__',
-	    'noecho',
-	    'noedit'
-	  ));
+    $website = trim(
+        getContent(
+            'event',
+            'display:detail',
+            'find:' . $_GET['slug'],
+            'show:__website__',
+            'noecho',
+            'noedit'
+        )
+    );
 
-	if ($website) {
-  	if (stripos($website,'http')===false) {
-    	$website = 'http://' . $website;
-  	}
-		header('Location:' . $website);
-	}
+    if ($website) {
+        if (stripos($website, 'http')===false) {
+            $website = 'http://' . $website;
+        }
+        header('Location:' . $website);
+    }
 
 ?>

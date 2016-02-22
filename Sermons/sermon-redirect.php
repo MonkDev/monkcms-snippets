16b82d402a8f7563ce629842b2af0a1bb4d55fc6
Chris Ullyott <chris@monkdevelopment.com>
2016-02-22 15:24:24 -0800

    Added sermon redirect for when custom sermon detail pages are used

---
+++ Sermons/sermon-redirect.php
@@ -0,0 +1,55 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/monkcms.php"); ?>
+<?php
+
+  /*
+    For ekk_eventpage.php
+    Redirects the default sermon detail URL to the custom detail page
+  */
+
+
+  /**
+   * Build a path from a list of parts
+   */
+  function path()
+  {
+    $path = '';
+    foreach (func_get_args() as $key => $p) {
+      if ($p && $key == 0) {
+        $path .= rtrim($p, '/') . '/';
+      } elseif ($p) {
+        $path .= trim($p, '/') . '/';
+      }
+    }
+    return $path;
+  }
+
+
+  /**
+   * Build a sermon detail page URL, knowing the custom sermons page
+   * (If the Page cannot be found, no redirect will be done)
+   */
+  function customSermonRedirect($sermonSlug, $pageId, $detailPath = '')
+  {
+    $domain = 'http://' . $_SERVER['HTTP_HOST'];
+    $sermonPage = getContent(
+      'page',
+      'display:detail',
+      'find:p-' . $pageId,
+      'show:__url__',
+      'noecho',
+      'noedit'
+    );
+    if ($sermonPage) {
+      header('Location:' . path($domain, $sermonPage, $detailPath, $sermonSlug));
+    } else {
+      return false;
+    }
+  }
+
+
+  /**
+   * Do the redirect
+   */
+  customSermonRedirect($_GET['sermonslug'], 637521, 'detail');
+
+?>

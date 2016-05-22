3bd83d5ea2d0df92cf26ad2aa94b00de64dd0efc
Chris Ullyott <chris@monkdevelopment.com>
2016-05-22 07:48:26 -0700

    Update dropdown script
    Includes tidying and addition of  argument.

--- Javascript/dropdowns/list_to_dropdown.js
+++ Javascript/dropdowns/list_to_dropdown.js
@@ -6,36 +6,36 @@
  *
  * Sets up select menus for long sidebar lists.
  * Demo: mcmsFilterSelect('#sermon_list_archive');
- *
- */ 
-function mcmsFilterSelect(list){
-    
-    $(list).each(function(){
-        var    ul = $(this),
-                id = ul.attr('id'),
-                items = $('li', this),
-                select = '',
-                select_options = '';
-        items.each(function(i){
-            var    a = $('> a', this),
-                    title = a.text(),
-                    url = a.attr('href');
-            var option = '<option value="'+url+'">'+title+'</option>';
-            select_options = select_options + option;
-            if (!--items.length){
-                select = select + '<p>';
-                select = select + '<select id="select_'+id+'" style="width:140px;">';
-                select = select + '<option value="">-</option>';
-                select = select + select_options;
-                select = select + '</select></div>';
-                select = select + '</p>';
-                ul.replaceWith(select);
-                $('body').on('change', '#select_' + id, function(){
+ */
+
+function mcmsFilterSelect(list, width) {
+    $(list).each(function() {
+        var $ul = $(this),
+            id = $ul.attr('id'),
+            items = $('li', this),
+            select = '',
+            options = '';
+
+        items.each(function() {
+            var $a = $('> a', this);
+            options += '<option value="' + $a.attr('href') + '">' + $a.text() + '</option>';
+
+            if (!--items.length) {
+                if (!width) width = 140;
+
+                select += '<p>';
+                select += '<select id="select_' + id + '" style="width:' + width + 'px;">';
+                select += '<option value="">-</option>';
+                select += options;
+                select += '</select>';
+                select += '</p>';
+
+                $ul.replaceWith(select);
+
+                $('body').on('change', '#select_' + id, function() {
                     window.location.href = $(this).val();
                 });
             }
         });
-        
     });
-    
 }

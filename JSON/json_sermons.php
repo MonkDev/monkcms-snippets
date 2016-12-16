e48fbb5ae8f966954b28ed44c5e8d4582f6e3c70
Chris Ullyott <chris@monkdevelopment.com>
2016-12-16 10:37:52 -0800

    Add new JSON sermon output script
    This script utilizes the new JSON output for getContent and supports parameters by query string.

--- JSON/json_sermons.php
+++ JSON/json_sermons.php
@@ -1,245 +1,91 @@
-<?php require($_SERVER["DOCUMENT_ROOT"]."/monkcms.php");
+<?php
 
-$outarray;
-$nodes;
-$json;
-
-$category = "";
-$series = "";
-$group = "";
-$howmany = ""; // if left blank the API will return 50
-$offset = "";
-$order = "recent";
-$hideseries="";
-$hidecategory="";
-$hidegroup="";
-$passage="";
-$preacher="";
-$tags="";
+/**
+ * MonkCMS JSON output for sermons
+ *
+ * Builds JSON output for sermons, supporting all getContent parameters via the
+ * query string, like so:
+ *
+ * http://example.com/json_sermons.php?find_series=he-is-risen
+ * http://example.com/json_sermons.php?find_group=east-campus&offset=10
+ *
+ * Chris Ullyott <chris@monkdevelopment.com>
+ */
 
 
-//$filters = json_decode(stripslashes(urldecode($_GET['filter'])),true);
-$filters = $_GET;
-if($filters != null){
-	foreach($filters as $key=>$value){
-		switch ($key)
-		{
-		case 'series':
-			$series = $value;
-			break;
-		case 'category':
-			$category = $value;
-			break;
-		case 'group':
-			$group = $value;
-			break;
-		case 'limit':
-			$howmany = $value;
-			break;
-		case 'order':
-			$order = $value;
-			break;
-		case 'hide_series':
-			$hideseries = $value;
-			break;
-		case 'hide_category':
-			$hidecategory = $value;
-			break;
-		case 'hide_group':
-			$hidegroup = $value;
-			break;
-		case 'preacher':
-			$preacher = $value;
-			break;
-		case 'tags':
-			$tags = $value;
-			break;
-		case 'passage':
-			$passage = $value;
-			break;
-		case 'offset':
-			$offset = $value;
-			break;
+// Include MonkCMS
+require($_SERVER["DOCUMENT_ROOT"] . '/monkcms.php');
 
-		}
-	}
-}
 
-$string = getContent(
-	"sermons",
-	"display:list",
-	"order:".$order,
-	"find_category:".$category,
-	"find_series:".$series,
-	"find_group:".$group,
-	"hide_series:".$hideseries,
-	"hide_category:".$hidecategory,
-	"hide_group:".$hidegroup,
-	"find_preacher:".$preacher,
-	"find_tag:".$tags,
-	"find_passage:".$passage,
-	"howmany:".$howmany,
-	"offset:".$offset,
-	"show:__title__",
-	"show:||",
-	"show:__imageurl width='320' height='150'__",
-	"show:||",
-	"show:__preview limit='200'__",
-	"show:||",
-	"show:__preacher__",
-	"show:||",
-	"show:__date format='M d Y'__",
-	"show:||",
-	"show:__summary__",
-	"show:||",
-	"show:__slug__",
-	"show:||",
-	"show:__audiourl__",
-	"show:||",
-	"show:__videourl__",
-	"show:||",
-	"show:__videoembed__",
-	"show:||",
-	"show:__imageurl2 width='50' height='50'__",
-	"show:||",
-	"show:__text__",
-	"show:||",
-	"show:__category__",
-	"show:||",
-	"show:__series__",
-	"show:||",
-	"show:__seriesimage width='480'__",
-	"show:||",
-	"show:__passage__",
-	"show:||",
-	"show:__passageslug__",
-	"show:||",
-	"show:__passagebook__",
-	"show:||",
-	"show:__passageverse__",
-	"show:||",
-	"show:__group__",
-	"show:||",
-	"show:__tags__",
-	"show:||",
-	"show:__notes__",
-	"show:||",
-	"show:__id__",
-	"show:~~~~",
-	"noecho"
-);
-
-//echo($string);
-
-$prearray = explode("~~~~",$string);
-
-for ($i=0; $i <count($prearray)-1; $i++) {
-	$outarray[$i] = explode("||",$prearray[$i]);
-}
-
-/* function getAudio($url){
-        $a = preg_match('@(url=http?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\S+(mp3|aac|wav|m4a))?)?)?)@', $url,$matches);
-    $audio = str_replace("url=","",$matches[0]);
-    return $audio;
-    }
-
-    function getVideo($url){
-         $v = preg_match('@(url=http?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\S+(mov|mp4|m4v))?)?)?)@', $url,$matches);
-       $video = str_replace("url=","",$matches[0]);
-       return $video;
-    }*/
-
-$i = 0;
-foreach ($outarray as $key => $value) {
-
-	preg_match_all('/(src)=("[^"]*")/i',$value[9],$parts);
-	$type;
-	$media_id = substr($parts[0][0],5,-1);
-	$posyt = strpos($parts[0][0],"youtube");
-	$posvm = strpos($parts[0][0],"vimeo");
-	//$iframe[0][2][0] gets src value from iframe
-	if($posyt !==false){
-		$type = "youtube";
-	}
-	if($posvm !==false){
-		$type = "vimeo";
-	}
-
-	$nodes[$i] = array(
-
-		title => $value[0],
-		image => $value[1],
-		preview => $value[2],
-		author => $value[3],
-		date => $value[4],
-		summary => $value[5],
-		slug => $value[6],
-		audio => $value[7],
-		video => $value[8],
-		embed => array(
-			type => $type,
-			media_id => $media_id,
-			code => $value[9]
-		),
-		thmb => $value[10],
-		text => $value[11],
-		category => $value[12],
-		series => $value[13],
-		seriesimage => $value[14],
-		passage => $value[15],
-		passageslug => $value[16],
-		passagebook => $value[17],
-		passageverse => $value[18],
-		group => $value[19],
-		tags => $value[20],
-		notes => $value[21],
-		id => $value[22]
-	);
-	$i++;
-}
-
-$totalpossible = getContent(
-	"sermons",
-	"display:list",
-	"order:".$order,
-	"find_category:".$category,
-	"find_series:".$series,
-	"find_group:".$group,
-	//"howmany:".$howmany, hide both howmany and offest because it needs to know total of possible articles to return.
-	"find_author:".$author,
-	"find_tag:".$tags,
-	"hide_series:".$hideseries,
-	"hide_category:".$hidecategory,
-	"hide_group:".$hidegroup,
-	"before_show:__totalpossible__",
-	"noecho"
+// Define default parameters
+$params = array(
+    'display' => 'list',
+    'order'   => 'recent',
+    'howmany' => 50
 );
 
 
+// Merge custom parameters from query string
+$params = array_merge($params, array_filter($_GET));
 
-$output = array(
-	items => $nodes,
-	sermonsReturned => $i,
-	totalSermons => intval($totalpossible),
-	currentOffset => $offset ? $offset : 0
+
+// Parse parameters for getContent
+$gcParams = array();
+foreach ($params as $k => $v) {
+    $gcParams[] = "{$k}:{$v}";
+}
+
+
+// Define custom formatted API tags to request
+$gcTags = array(
+    "date format='r'",
+    "dateTwo format='Y-m-d'",
+    "dateThree format='F j, Y'",
+    "preview limit='200'",
+    "imageurl width='1280' height='720'",
+    "imageurl2 width='200' height='200'",
 );
 
 
-//$output = array("items" => $nodes);
-
-$json = json_encode($output);
-//print_r($articles);
-
-$callback = $_REQUEST['callback'];
-
-if($callback){
-	header('Content-type: text/javascript');
-	//echo("jsonp");
-	echo $callback . '(' . $json . ');';
-}else{
-	header('Content-type: application/json');
-	//echo "no-jibber";
-	echo $json;
+// Build the arguments for getContent
+$gcArgs = array('sermons');
+$gcArgs = array_merge($gcArgs, $gcParams);
+foreach ($gcTags as $key => $tag) {
+    $gcArgs[] = "show:__{$tag}__";
 }
+$gcArgs[] = 'json';
 
-?>
+
+// Make API request
+$response = call_user_func_array('getContent', $gcArgs);
+$items = $response['show'];
+
+
+// Make second request to get total number of possible items
+$gcArgs = array('sermons');
+$gcArgs = array_merge($gcArgs, $gcParams);
+$gcArgs[] = 'before_show:__totalpossible__';
+$gcArgs[] = 'noecho';
+$gcArgs[] = 'noedit';
+$total = call_user_func_array('getContent', $gcArgs);
+
+
+// Build json
+$json = json_encode(array(
+    'total'  => intval($total),
+    'count'  => count($items),
+    'offset' => !empty($_GET['offset']) ? intval($_GET['offset']) : 0,
+    'items'  => $items
+));
+
+
+// Output
+$callback = !empty($_REQUEST['callback']) ? $_REQUEST['callback'] : null;
+
+if ($callback) {
+    header('Content-type: text/javascript');
+    echo $callback . '(' . $json . ');';
+} else {
+    header('Content-type: application/json');
+    echo $json;
+}

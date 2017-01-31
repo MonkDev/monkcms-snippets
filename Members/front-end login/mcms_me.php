4a29b675e722b7c42791a09907fc91fe4d9dfdea
Chris Ullyott <chrisullyott@users.noreply.github.com>
2017-01-31 09:13:30 -0800

    Update mcms_me.php

--- Members/front-end login/mcms_me.php
+++ Members/front-end login/mcms_me.php
@@ -9,13 +9,12 @@
 	  'nocache'
 	);
 
-	// display a relevant message depending on whether the user is viewing a transaction receipt
-	// or if they are viewing the confirmation page after completing an order.
+	// Order history link
 	if ($_GET['view'] == 'orders') {
 	  $backLink = '<a href="/me/">Order history</a> | ';
 	}
 
-	// displays a links to edit the user's account or logo out
+	// Edit account + log out links
 	getContent(
 	  'member',
 	  'display:summary',
@@ -39,7 +38,7 @@
 
 <?php
 
-	// grabs a list of links to transactions that have been made on the account. If the user arrives
+	// Lists transactions that have been made on the account. If the user arrives
 	// at the page from completing an order, it displays the order confirmation page.
 	$orderHistory = getContent(
 	  'member',

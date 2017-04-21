5d95fc63438a29fcbf29e99a990b6ca742b6b699
Chris Ullyott <chris@monkdevelopment.com>
2017-04-21 08:34:46 -0700

    Add maintenance splash page template

---
+++ htaccess/maintenance/README.md
@@ -0,0 +1,30 @@
+Maintenance Splash Page
+-----------------------
+
+Redirect with htaccess like:
+
+```
+RewriteCond %{REQUEST_URI} !\.(jpg|png|css|js)$
+RewriteCond %{REQUEST_URI} !^/maintenance\/?$
+RewriteRule ^(.*)$ /maintenance/? [R=307,L]
+```
+
+Add your IP address as an additional RewriteCond to avoid being redirected while letting the rest of the world redirect.
+
+```
+RewriteCond %{REMOTE_ADDR} !^YOUR_IP_ADDRESS
+```
+
+You can find out what your own IP address is with sites like:
+
+http://www.whatsmyip.org/
+
+The full block of htaccess might look like:
+
+```
+RewriteCond %{REMOTE_ADDR} !^50.26.158.9
+RewriteCond %{REMOTE_ADDR} !^68.224.168.150
+RewriteCond %{REQUEST_URI} !\.(jpg|png|css|js)$
+RewriteCond %{REQUEST_URI} !^/maintenance\/?$
+RewriteRule ^(.*)$ /maintenance/? [R=307,L]
+```
---
+++ htaccess/maintenance/index.php
@@ -0,0 +1,42 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/monkcms.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  <meta name="robots" content="noindex, nofollow">
+
+  <title><?= $MCMS_SITENAME ?></title>
+
+  <link href="http://fonts.googleapis.com/css?family=Kotta+One%7CCantarell:400,700" rel="stylesheet" type="text/css">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <link rel="stylesheet" href="_css/styles.css" />
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="_js/site.js"></script>
+</head>
+<body>
+
+  <header class="container">
+    <section class="content">
+      
+    <?php
+      getContent( 
+        "section",
+        "display:detail",
+        "find:maintenance-content",
+        "show:__text__"
+      );
+    ?>
+
+    </section>
+  </header>
+
+<?php getContent('site', 'display:detail', 'show:__trackingcode__'); ?>
+
+</body>
+</html>

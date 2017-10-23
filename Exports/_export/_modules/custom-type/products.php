77828cb55f0a8d5a3a3a5f056e8c8143d5bde7a0
Skyler Katz <skyler@monkdevelopment.com>
2017-10-23 10:45:06 -0700

    PSR2 Formatting

--- Exports/_export/_modules/custom-type/products.php
+++ Exports/_export/_modules/custom-type/products.php
@@ -1,129 +1,127 @@
 <?php
 
-	/*
+    /*
 
-	OUTPUT PRODUCTS AS CSV
+    OUTPUT PRODUCTS AS CSV
 
-	*/
+    */
 
-	require('../../_inc/config.php');
+    require('../../_inc/config.php');
 
-	set_time_limit(0);
+    set_time_limit(0);
 
     $filename = getSiteId() . '_' . 'products' . 'Export' . date('M') . '_' . date('d') . '_' . date('Y');
 
     // Header
-	header("Content-type: text/csv");
-	header("Content-Disposition: attachment; filename=" . $filename . ".csv");
-	header("Pragma: no-cache");
-	header("Expires: 0");
-	
-	// Functions
-	function processItem($in)
-	{
-		$out = trim($in);
-		$out = str_replace('"','""',$out);
-		$out = str_replace('&amp;','&',$out);
-		$out = '"' . $out . '"';
+    header("Content-type: text/csv");
+    header("Content-Disposition: attachment; filename=" . $filename . ".csv");
+    header("Pragma: no-cache");
+    header("Expires: 0");
 
-		return $out;
-	}
+    // Functions
+    function processItem($in)
+    {
+        $out = trim($in);
+        $out = str_replace('"', '""', $out);
+        $out = str_replace('&amp;', '&', $out);
+        $out = '"' . $out . '"';
 
-	$howmany = 10000; // Set to number of products in the module
+        return $out;
+    }
 
-	// Headers
-	$headers = '';
-	$headers .= '"Product Code",';
-	$headers .= '"Product Name",';
-	$headers .= '"Author",';
-	$headers .= '"Current Price",';
-	$headers .= '"Original Price",';
-	$headers .= '"Type",';
-	$headers .= '"Publisher",';
-	$headers .= '"Reference",';
-	$headers .= '"Program Affiliation",';
-	$headers .= '"Description",';
-	$headers .= '"Image1",';
-	$headers .= '"Image2",';
-	$headers .= '"Image3"';
-	$headers .= "\n";
+    $howmany = 10000; // Set to number of products in the module
 
-	// Lines
-	$batch_length = 100;
-	$batch_count = ceil($howmany / $batch_length);
+    // Headers
+    $headers = '';
+    $headers .= '"Product Code",';
+    $headers .= '"Product Name",';
+    $headers .= '"Author",';
+    $headers .= '"Current Price",';
+    $headers .= '"Original Price",';
+    $headers .= '"Type",';
+    $headers .= '"Publisher",';
+    $headers .= '"Reference",';
+    $headers .= '"Program Affiliation",';
+    $headers .= '"Description",';
+    $headers .= '"Image1",';
+    $headers .= '"Image2",';
+    $headers .= '"Image3"';
+    $headers .= "\n";
 
-	$get_products = '';
+    // Lines
+    $batchLength = 100;
+    $batchCount = ceil($howmany / $batchLength);
 
-	for ($i=1; $i<=$batch_count; $i++) {
+    $getProducts = '';
 
-		$this_howmany = strval($batch_length);
-		$this_offset = strval(($batch_length * ($i-1)));
+    for ($i=1; $i<=$batchCount; $i++) {
+        $thisHowmany = strval($batchLength);
+        $thisOffset = strval(($batchLength * ($i-1)));
 
-		$get_products .=
-		getContent(
-			"products",
-			"display:list",
-			'order:title',
-			'product:all',
-			"howmany_product:" . $this_howmany,
-			"offset_product:" . $this_offset,
-			'show_productlist:__productcode__',
-			'show_productlist:~||~',
-			'show_productlist:__producttitle__',
-			'show_productlist:~||~',
-			'show_productlist:__skuauthor__',
-			'show_productlist:~||~',
-			'show_productlist:__productprice__',
-			'show_productlist:~||~',
-			'show_productlist:__productOriginalprice__',
-			'show_productlist:~||~',
-			'show_productlist:__type__',
-			'show_productlist:~||~',
-			'show_productlist:__publisher__',
-			'show_productlist:~||~',
-			'show_productlist:__reference__',
-			'show_productlist:~||~',
-			'show_productlist:__affiliation__',
-			'show_productlist:~||~',
-			'show_productlist:__productdescription__',
-			'show_productlist:~||~',
-			'show_productlist:__productimageURL__',
-			'show_productlist:~||~',
-			'show_productlist:__productimageURL2__',
-			'show_productlist:~||~',
-			'show_productlist:__productimageURL3__',
-			'show_productlist:~|~|~',
-			'noecho'
-		);
-	}
+        $getProducts .=
+        getContent(
+            "products",
+            "display:list",
+            'order:title',
+            'product:all',
+            "howmany_product:" . $thisHowmany,
+            "offset_product:" . $thisOffset,
+            'show_productlist:__productcode__',
+            'show_productlist:~||~',
+            'show_productlist:__producttitle__',
+            'show_productlist:~||~',
+            'show_productlist:__skuauthor__',
+            'show_productlist:~||~',
+            'show_productlist:__productprice__',
+            'show_productlist:~||~',
+            'show_productlist:__productOriginalprice__',
+            'show_productlist:~||~',
+            'show_productlist:__type__',
+            'show_productlist:~||~',
+            'show_productlist:__publisher__',
+            'show_productlist:~||~',
+            'show_productlist:__reference__',
+            'show_productlist:~||~',
+            'show_productlist:__affiliation__',
+            'show_productlist:~||~',
+            'show_productlist:__productdescription__',
+            'show_productlist:~||~',
+            'show_productlist:__productimageURL__',
+            'show_productlist:~||~',
+            'show_productlist:__productimageURL2__',
+            'show_productlist:~||~',
+            'show_productlist:__productimageURL3__',
+            'show_productlist:~|~|~',
+            'noecho'
+        );
+    }
 
-	$get_products_array = explode("~|~|~", $get_products);
+    $getProductsArray = explode("~|~|~", $getProducts);
 
-	$lines = '';
+    $lines = '';
 
-	for ($i=0;$i<count($get_products_array)-1;$i++) {
+    for ($i=0;$i<count($getProductsArray)-1;$i++) {
+        $productArray = explode("~||~", $getProductsArray[$i]);
 
-		$product_array = explode("~||~",$get_products_array[$i]);
+        $line =
+        processItem($productArray[0]) . "," .
+        processItem($productArray[1]) . "," .
+        processItem($productArray[2]) . "," .
+        processItem($productArray[3]) . "," .
+        processItem($productArray[4]) . "," .
+        processItem($productArray[5]) . "," .
+        processItem($productArray[6]) . "," .
+        processItem($productArray[7]) . "," .
+        processItem($productArray[8]) . "," .
+        processItem($productArray[9]) . "," .
+        processItem($productArray[10]) . "," .
+        processItem($productArray[11]) . "," .
+        processItem($productArray[12]) . "\n";
 
-		$line =
-		processItem($product_array[0]) . "," .
-		processItem($product_array[1]) . "," .
-		processItem($product_array[2]) . "," .
-		processItem($product_array[3]) . "," .
-		processItem($product_array[4]) . "," .
-		processItem($product_array[5]) . "," .
-		processItem($product_array[6]) . "," .
-		processItem($product_array[7]) . "," .
-		processItem($product_array[8]) . "," .
-		processItem($product_array[9]) . "," .
-		processItem($product_array[10]) . "," .
-		processItem($product_array[11]) . "," .
-		processItem($product_array[12]) . "\n";
+        $lines .= $line;
+    }
 
-		$lines .= $line;
-	}
+    $lines = trim($lines, "\n");
 
-	$lines = trim($lines, "\n");
-
-	// Output
-	echo $headers . $lines;
+    // Output
+    echo $headers . $lines;
